2d469756fe2dae87649ed999dbfdd49221f5ce72
extuki <dombek.prokop@centrum.cz>
2022-11-25 12:39:39 +0100

    added and commited this project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+//Namespace
+namespace NumberGuesser
+{
+    // Main Class
+    internal class Program
+    {   // Entry Point Method
+        static void Main(string[] args)
+        {
+            string name = ("Prokop Dombek");
+            int age = 31;
+
+            // Start Here //
+            Console.WriteLine(name+ " is " +age);
+            Console.WriteLine("{0} is {1}", name, age);
+        }
+    }
+}
